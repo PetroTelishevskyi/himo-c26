a962fdbc68012ff95e6c03eebc6e94ae2f0e0888
Petro Telishevskyi <ptelishewsky@gmail.com>
2023-02-24 16:56:52 +0200

    Add itro section and adaptive this section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ if (iconMenu) {
         document.body.classList.toggle('_lock');
     });
 };
+
+const intrSwiper = new Swiper('.intro__swiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    autoHeight: true,
+    keyboard: {
+        enable:true,
+        onlyInViewport: true,
+        pageDown:true
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    speed: 800,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    allowTouchMove: false,
+});
